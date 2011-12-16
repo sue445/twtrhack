3aabdbcf6b330f39dc95f339e5d2acaffd810d27
sue445 <sue445@sue445.net>
2011-12-17 01:28:26 +0900

    create test case

--- src/main/java/net/sue445/GreetingBot.java
+++ src/main/java/net/sue445/GreetingBot.java
@@ -2,4 +2,12 @@ package net.sue445;
 
 public class GreetingBot {
 
+	/**
+	 * フォローしてる人が「ただいま」とツイートしたら「おかえり」と言う
+	 * @return
+	 */
+	public String okaeri(){
+		return "";
+	}
+
 }
--- src/test/java/net/sue445/GreetingBotTest.java
+++ src/test/java/net/sue445/GreetingBotTest.java
@@ -1,11 +1,19 @@
 package net.sue445;
 
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class GreetingBotTest{
 
+	private GreetingBot greetingBot = new GreetingBot();
+
+
 	@Test
-	public void test() {
+	public void okaeri() {
+		String actual = greetingBot.okaeri();
+		assertThat(actual, is("おかえり RT @sue445 借り暮らしのただいまってぃ"));
 	}
 
 }
