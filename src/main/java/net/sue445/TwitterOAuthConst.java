1af4d9b90fba561a51b58d03edbd22fb0c8052e9
sue445 <sue445@sue445.net>
2011-12-17 01:44:46 +0900

    add constant template

---
+++ src/main/java/net/sue445/TwitterOAuthConst.java
@@ -0,0 +1,8 @@
+package net.sue445;
+
+public class TwitterOAuthConst {
+	public static final String O_AUTH_CONSUMER_KEY = "";
+	public static final String O_AUTH_CONSUMER_SECRET = "";
+	public static final String O_AUTH_ACCESS_TOKEN = "";
+	public static final String O_AUTH_ACCESS_TOKEN_SECRET = "";
+}
